a59f0896d3c7860b34a16c9aebddc17e93731dd1
Erik Åkerberg <e.akerberg@hotmail.com>
2020-04-17 17:23:32 +0200

    Refaktorerat InitialConnectionWindow

--- src/gui/InitialConnectionWindow.java
+++ src/gui/InitialConnectionWindow.java
@@ -4,70 +4,74 @@ import connection.ConnectionHandler;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class InitialConnectionWindow extends JFrame {
 
+    private ConnectionHandler connectionHandler;
+
+    private final String HOST_GAME_BTN_TXT = "Host local game";
+    private final String JOIN_GAME_BTN_TXT = "Join local game";
+
+    private class BtnListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            boolean hostGameBtnClicked = e.getActionCommand().equals(HOST_GAME_BTN_TXT);
+
+            try {
+                connectionHandler.initializeConnection(hostGameBtnClicked);
+                InitialConnectionWindow.this.setVisible(false);
+            } catch (IOException ioe) {
+                JOptionPane.showMessageDialog(InitialConnectionWindow.this,
+                        hostGameBtnClicked ? "Local game is already running" :  "Could not find existing local game",
+                        "Error!", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
     public InitialConnectionWindow(ConnectionHandler connectionHandler) {
 
         super("Battleships");
+        this.connectionHandler = connectionHandler;
+
         setLayout(new BorderLayout());
         setSize(300, 200);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
 
-        JPanel panel = setUpWindowContent(connectionHandler);
+        JPanel panel = setUpWindowContent();
         add(panel);
 
         setVisible(true);
     }
 
-    private JPanel setUpWindowContent(ConnectionHandler connectionHandler){
+    private JPanel setUpWindowContent() {
 
-       JPanel buttonPanel = new JPanel();
+        JPanel buttonPanel = new JPanel();
 
-       addConnectionButtons(buttonPanel, connectionHandler);
+        addConnectionButtons(buttonPanel);
 
         return buttonPanel;
     }
 
-    private void addConnectionButtons(JPanel buttonPanel, ConnectionHandler connectionHandler){
+    private void addConnectionButtons(JPanel buttonPanel) {
 
-        JButton connectToRemoteServerBtn = new JButton("Join local game");
+        JButton connectToRemoteServerBtn = new JButton(JOIN_GAME_BTN_TXT);
         buttonPanel.add(connectToRemoteServerBtn);
 
-        connectToRemoteServerBtn.addActionListener(e -> {
-            boolean connectionSuccess = true;
-            try{connectionHandler.initializeConnection(false);
-
-            }catch(IOException ioe){
-                connectionSuccess = false;
-                System.out.println("hittade inget lokalt spel");
-                JOptionPane.showMessageDialog(this,"Could not find existing local game", "Error!",JOptionPane.ERROR_MESSAGE);
-            }
-            //Om boolean...
-            if(connectionSuccess){
-                setVisible(false);
-            }
-        });
-
-        JButton createServerBtn = new JButton("Host local game");
+        JButton createServerBtn = new JButton(HOST_GAME_BTN_TXT);
         buttonPanel.add(createServerBtn);
-        createServerBtn.addActionListener(e -> {
-            boolean connectionSuccess = true;
-            try{connectionHandler.initializeConnection(true);
 
-            }catch(IOException ioe){
-                System.out.println("fel i göra egen server");
-                connectionSuccess = false;
-                JOptionPane.showMessageDialog(this,"Local game already exists. Why not join it?", "Error!",JOptionPane.ERROR_MESSAGE);
-            }
-            if(connectionSuccess){
-                setVisible(false);
-            }
+        BtnListener listener = new BtnListener();
 
-        });
+        connectToRemoteServerBtn.addActionListener(listener);
+        createServerBtn.addActionListener(listener);
+
 
     }
 
